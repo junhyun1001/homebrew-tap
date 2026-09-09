c608520f86056a16fdd082ce6abf2fea1829f5c6
junhyun1001 <junhyun1001@gmail.com>
2026-09-10 03:20:41 +0900

    chore: bump swapcomment-for-youtube to 1.0.1

--- Casks/swapcomment-for-youtube.rb
+++ Casks/swapcomment-for-youtube.rb
@@ -1,5 +1,5 @@
 cask "swapcomment-for-youtube" do
-  version "1.0.0"
+  version "1.0.1"
   sha256 "36bdb519cf25d92316f7ef5f1f55b2c1dd10a0e475669ab005969bf38af1f50d"
 
   url "https://github.com/junhyun1001/SwapComment-for-YouTube/releases/download/v#{version}/SwapComment-for-YouTube.zip"
