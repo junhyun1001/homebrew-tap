38410191f744b49d12f5e1412a026809bc21bff8
junhyun1001 <junhyun1001@gmail.com>
2026-09-09 15:36:04 +0900

    feat: add swapcomment-for-youtube cask v1.0.0

---
+++ Casks/swapcomment-for-youtube.rb
@@ -0,0 +1,18 @@
+cask "swapcomment-for-youtube" do
+  version "1.0.0"
+  sha256 "36bdb519cf25d92316f7ef5f1f55b2c1dd10a0e475669ab005969bf38af1f50d"
+
+  url "https://github.com/junhyun1001/SwapComment-for-YouTube/releases/download/v#{version}/SwapComment-for-YouTube.zip"
+  name "SwapComment for YouTube"
+  desc "Lightweight native macOS client that brings YouTube comments to sidebar"
+  homepage "https://github.com/junhyun1001/SwapComment-for-YouTube"
+
+  depends_on macos: ">= :ventura"
+
+  app "SwapComment for YouTube.app"
+
+  zap trash: [
+    "~/Library/Saved Application State/com.junhyun.SwapCommentForYouTube.savedState",
+    "~/Library/WebKit/com.junhyun.SwapCommentForYouTube",
+  ]
+end
